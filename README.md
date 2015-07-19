70dc2c46ee641dbd9be6a7da42586dc5f329549f
Benjamin Boruff <benboruff@gmail.com>
2015-07-19 16:10:27 -0400

    basic APOD query and display

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#An Angular 1.4.x NASA APOD viewer SPA
+##An Angular 1.4.x NASA APOD viewer SPA
 
---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
+(function () {
+	'use strict';
+	angular.module('app', [])
+		.service('apodService', ['$http', function ($http) {
+			this.get = function () {
+				console.log('Hello, from apodService!');
+				return $http.get('https://api.data.gov/nasa/planetary/apod?api_key=DEMO_KEY&format=JSON')
+					.then(function (res) {
+						return res.data;
+					},
+						function (err) {
+							return {
+								message: "There has been an error in your request. Please try again later.",
+								error: "Admins are on it!"
+							};
+						});
+			};
+		}])
+		.controller('ApodviewerCtrl', ['apodService', function (apodService) {
+			var vm = this;
+			vm.getApodData = function () {
+				apodService.get()
+					.then(function (apodData) {
+						vm.apodData = apodData;
+						vm.video = false;
+						if (vm.apodData.media_type === 'video') {
+							vm.video = true;
+						}
+						console.log(vm.video);
+						console.log(vm.apodData.url);
+					});
+			};
+			vm.getApodData();
+		}])
+		.filter('trusted', ['$sce', function ($sce) {
+			return function (url) {
+				return $sce.trustAsResourceUrl(url);
+			};
+		}]);
+})();
