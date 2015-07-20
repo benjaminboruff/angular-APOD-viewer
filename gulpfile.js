be31aee2c53e4bb48684282850ed05a5cae0a8b9
Benjamin Boruff <benboruff@gmail.com>
2015-07-19 20:41:59 -0400

    added concat,minify,build tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,42 @@ var gulp = require('gulp'),
         concat = require('gulp-concat'),
         uglify = require('gulp-uglify'),
         rename = require('gulp-rename'),
-          maps = require('gulp-sourcemaps'),
-           del = require('del');
+        maps = require('gulp-sourcemaps'),
+        del = require('del');
 
-gulp.task('lint', function() {
-  return gulp.src('./src/js/main.js')
-    .pipe(lint())
-    .pipe(lint.reporter('default'));
+gulp.task('lint', function () {
+        return gulp.src('src/js/main.js')
+                .pipe(lint())
+                .pipe(lint.reporter('default'));
+});
+
+gulp.task('concatScripts', function () {
+        return gulp.src([
+                'src/bower_components/angular/angular.js',
+                'src/js/main.js'])
+                .pipe(maps.init())
+                .pipe(concat('app.js'))
+                .pipe(maps.write('./'))
+                .pipe(gulp.dest('src/js'));
+});
+
+gulp.task('minifyScripts', ['concatScripts'], function () {
+        return gulp.src('src/js/app.js')
+                .pipe(uglify())
+                .pipe(rename({
+                        suffix: ".min",
+                        extname: ".js"
+                }))
+                .pipe(gulp.dest('src/js'));
+
+});
+
+gulp.task('build', ['minifyScripts'], function () {
+        return gulp.src(['src/js/app.min.js',
+                'src/index.html'], {base: './src/'})
+                .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function () {
+        console.log("not yet defined");
 });
