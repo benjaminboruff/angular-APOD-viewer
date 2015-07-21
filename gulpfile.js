093d4537fd679355e74ecaa1d240b84937deb954
Benjamin Boruff <benboruff@gmail.com>
2015-07-20 21:08:01 -0400

    added watch to gulp tasks, added api_key change to form

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var gulp = require('gulp'),
-		      lint = require('gulp-jshint'),
-        concat = require('gulp-concat'),
-        uglify = require('gulp-uglify'),
-        rename = require('gulp-rename'),
-        maps = require('gulp-sourcemaps'),
-        del = require('del');
+var  gulp = require('gulp'),
+     lint = require('gulp-jshint'),
+   concat = require('gulp-concat'),
+   uglify = require('gulp-uglify'),
+   rename = require('gulp-rename'),
+     maps = require('gulp-sourcemaps'),
+      del = require('del');
 
 gulp.task('lint', function () {
         return gulp.src('src/js/main.js')
@@ -37,8 +37,13 @@ gulp.task('minifyScripts', ['concatScripts'], function () {
 
 gulp.task('build', ['minifyScripts'], function () {
         return gulp.src(['src/js/app.min.js',
-                'src/index.html'], {base: './src/'})
+                'src/index.html'], { base: './src/' })
                 .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+        gulp.watch('src/js/main.js', ['lint']);
+        gulp.watch('src/js/main.js', ['concatScripts']);
 });
 
 gulp.task('default', function () {
--- src/js/app.js
+++ src/js/app.js
@@ -28371,32 +28371,45 @@ var minlengthDirective = function() {
 				var apodUrl = "https://api.data.gov/nasa/planetary/apod?api_key=" + apikey + "&format=JSON";
 				console.log('Hello, from apodService!');
 				console.log(apikey);
-				return $http.get(apodUrl)
-					.then(function (res) {
-						return res.data;
-					},
-						function (err) {
-							return {
-								message: "There has been an error in your request. Please try again later.",
-								error: "Admins are on it!"
-							};
-						});
+				return $http.get(apodUrl, { cache: true })
+					.success(function (data, status, headers, config) {
+						console.log("HERE!");
+						console.log(headers());
+						return {
+							"data": data,
+							"headers": headers
+						};
+					})
+					.error(function (data, status, headers, config) {
+						return {
+							message: "There has been an error in your request. Please try again later.",
+							error: "Admins are on it!"
+						};
+					});
 			};
 		}])
 		.controller('ApodviewerCtrl', ['apodService', function (apodService) {
 			var vm = this;
 			vm.apikey = "DEMO_KEY";
+			vm.altkey = "";
 			vm.getApodData = function () {
 				apodService.get(vm.apikey)
-					.then(function (apodData) {
-						vm.apodData = apodData;
+					.then(function (apodObj) {
+						vm.apodData = apodObj.data;
+						vm.apodHeaders = apodObj.headers;
 						vm.video = false;
 						if (vm.apodData.media_type === 'video') {
 							vm.video = true;
 						}
 						console.log(vm.video);
-						console.log(vm.apodData.url);
+						console.log(vm.apodData);
+						console.log(vm.apodHeaders('X-RateLimit-Remaining'));
 					});
+			};
+			vm.update = function (key) {
+				vm.apikey = key;
+				console.log(vm.apikey);
+				vm.getApodData();
 			};
 			vm.getApodData();
 		}])
